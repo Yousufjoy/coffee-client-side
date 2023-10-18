7e4517947e794c9489667265e793de63e2530b53
Yousufjoy <kanekiken333@gmail.com>
2023-10-18 18:37:30 +0600

    Create Add coffee from Client Side and get data

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,28 +1,101 @@
 const AddCoffee = () => {
+  const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const photo = form.photo.value;
+
+    const newCoffee = { name, quantity, supplier, taste, category, photo };
+
+    console.log(newCoffee);
+  };
+
   return (
-    <>
-      <h2>Add a coffee!</h2>
-      <form>
-        <div className=" flex ">
-          <div className="join">
+    <div className=" bg-[#F5F3F0] p-24">
+      <h2 className=" text-3xl font-extrabold">Add a coffee!</h2>
+      <form onSubmit={handleAddCoffee}>
+        {/* Name And quantity Row */}
+        <div className=" md:flex mb-8">
+          <div className="join md:w-1/2">
             <input
               type="text"
-              className="input input-bordered join-item"
+              name="name"
+              className="input input-bordered join-item w-full"
               placeholder="Name"
             />
-            <button className="btn join-item rounded-r-full">Add</button>
           </div>
-          <div className="join">
+          <div className="join md:w-1/2 ml-4">
             <input
               type="text"
-              className="input input-bordered join-item"
+              name="quantity"
+              className="input input-bordered join-item w-full"
               placeholder="Quantity"
             />
-            <button className="btn join-item rounded-r-full">Add</button>
           </div>
         </div>
+        {/*Supplier Row */}
+        <div className=" md:flex mb-8">
+          <div className="join md:w-1/2">
+            <input
+              type="text"
+              name="supplier"
+              className="input input-bordered join-item w-full"
+              placeholder="Supplier Name"
+            />
+          </div>
+          <div className="join md:w-1/2 ml-4">
+            <input
+              type="text"
+              name="taste"
+              className="input input-bordered join-item w-full"
+              placeholder="Taste"
+            />
+          </div>
+        </div>
+        {/* Category and details row */}
+        <div className=" md:flex mb-8">
+          <div className="join md:w-1/2">
+            <input
+              type="text"
+              name="category"
+              className="input input-bordered join-item w-full"
+              placeholder="Category"
+            />
+          </div>
+          <div className="join md:w-1/2 ml-4">
+            <input
+              type="text"
+              name="details"
+              className="input input-bordered join-item w-full"
+              placeholder="Details"
+            />
+          </div>
+        </div>
+        {/* form photo url row */}
+        <div className="">
+          <div className="join w-full">
+            <input
+              type="text"
+              name="photo"
+              className="input input-bordered join-item w-full"
+              placeholder="Photo URL"
+            />
+          </div>
+        </div>
+        <div className=" mt-5">
+          <input
+            type="submit"
+            value="Add Coffee"
+            className="btn btn-block bg-slate-700 text-white"
+          />
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
