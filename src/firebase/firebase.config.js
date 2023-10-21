85dd3b691c91bcce335e2d41343e2b77fceb2bc6
Yousufjoy <kanekiken333@gmail.com>
2023-10-21 21:03:44 +0600

    added firebase to the project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDX0xkp6x04ChzQblE1m3i5GnMV3U8o2jA",
+  authDomain: "coffee-store-7db99.firebaseapp.com",
+  projectId: "coffee-store-7db99",
+  storageBucket: "coffee-store-7db99.appspot.com",
+  messagingSenderId: "792887644368",
+  appId: "1:792887644368:web:94db7e2de358de34d68ed5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
